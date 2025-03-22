8f15fac766e232edea4b55dcdc1308aab505cc55
cinamonydh <cinamonydh@gmail.com>
2025-03-22 18:48:36 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd01 test
+          Learn GithubAction cicd01 test2
         </a>
       </header>
     </div>
